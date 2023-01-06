f2ac7a08037b514faa74131f8f477724132dc04d
bkramarski <bkramarski@gmail.com>
2023-01-06 19:27:51 +0100

    Fix for the build time dependency

--- activate.sh
+++ activate.sh
@@ -16,4 +16,7 @@ fi
 HERE="$(dirname "$(readlink -f "${0}")")"
 mkdir -p "${HERE}/database/pgdata"
 
+# During the build, postgres image takes something out of qcadoo, we need to have it first
+docker-compose  -f "${HERE}/docker-compose.yml" build qcadoo
+
 exec docker-compose  -f "${HERE}/docker-compose.yml" up --build -d
